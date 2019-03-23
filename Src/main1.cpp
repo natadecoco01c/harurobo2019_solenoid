a78e4f48e3e30e8dba9009a5ff28b5e97d101fed
natadecoco01c <natadecoco01c@gmail.com>
2019-03-23 15:20:37 +0900

    とりあえずコメントアウト部分消して綺麗にした

--- Src/main1.cpp
+++ Src/main1.cpp
@@ -4,6 +4,7 @@
  *  Created on: 2019/02/27
  *      Author: natadecoco01c
  */
+// 0x301 CAN受信
 #include <main1.h>
 #include <array>
 #include "SolenoidNode.hpp"
@@ -22,7 +23,7 @@ uint8_t once = 0;
 uint8_t order;
 uint8_t endis = 0;
 
-template<typename T> //����͊֐��e���v���[�g�Ƃ������̂炵��
+template<typename T>
 static void can_unpack(const uint8_t (&buf)[CAN_MTU], T &data);
 template<typename T>
 static void can_pack(uint8_t (&buf)[CAN_MTU], const T data);
@@ -34,7 +35,6 @@ static void _Error_Handler(void);
 static void MX_TIM2_Init(void);
 
 static SolenoidNode * const solenoidNode = new SolenoidNode();
-//static CarrierNode * const carrierNode = new CarrierNode();
 
 int Counter_1 = 0;
 int Counter_2 = 0;
@@ -48,7 +48,7 @@ uint8_t tx_payload8[CAN_MTU];
 uint8_t tx_payload9[CAN_MTU];
 uint8_t timetosend = 0;
 //static constexpr uint16_t id_handStatus = 0x101;
-static constexpr uint16_t id_handCmd = 0x102;
+//static constexpr uint16_t id_handCmd = 0x102;
 
 uint8_t cmd[CAN_MTU]; //受信データ格納用
 
@@ -78,9 +78,7 @@ int main1(void) {
 	HAL_Delay(100);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
 
-	can_enable(); //CAN�̗L����
-
-	// loop forever
+	can_enable();
 
 	tx_msg8.StdId = 0x100;
 	tx_msg8.RTR = CAN_RTR_DATA;
@@ -91,22 +89,9 @@ int main1(void) {
 	tx_msg9.IDE = CAN_ID_STD;
 	tx_msg9.DLC = 1;
 
-//	uint32_t last_stat_time = HAL_GetTick();
-//	const uint32_t stat_interval = 1000 / 100; //分母がヘルツに
-
 	HAL_NVIC_EnableIRQ (TIM2_IRQn);
 	HAL_TIM_Base_Start_IT(&htim2);
 
-//	NVIC_SetPriority(EXTI9_5_IRQn, 1);
-//	HAL_NVIC_SetPriority(EXTI9_5_IRQn, 1U, 1U);
-//	NVIC_EnableIRQ (EXTI9_5_IRQn);
-//	HAL_NVIC_EnableIRQ (EXTI9_5_IRQn);
-
-//status = can_rx(&rx_msg, 3);
-
-	//nhk�̕��ł͏��CAN����̐M�����瓮���Ă����ǁA�����͂����Ȃ蓮���o���̂ŁA�ŏ��ɏ�����(�L����)���K�v
-//	solenoidNode->SetCommand(SolenoidCommands::reset_cmd);
-//	solenoidNode->Control();
 	while (1) {
 		now_pattern = 0b00000000;
 		solenoid_drive(now_pattern);
@@ -130,284 +115,17 @@ int main1(void) {
 
 			}
 		}
-		while (endis == 1) {
-//	solenoid_enable();
-//	solenoid_drive(now_pattern);
-			solenoid_drive(0x00);
-//	HAL_Delay(1000);
-//	solenoid_drive(0xff);
 
-			//carrierNode->SetCommand(CarrierCommands::reset_cmd);
-			//carrierNodse->Control();
-
-			//solenoidNode->SetCommand(SolenoidCommands::apploaching_s_cmd); //三宝接近というか三宝つかむようのアームを下げる部分　0b0000 0010正論
-			//solenoidNode->Control();
-			now_pattern &= ~0b00000001;
-			now_pattern |= 0b00100010;
-			solenoid_drive(now_pattern);
-			//solenoidNode->SetCommand(SolenoidCommands::apploaching_a_cmd); //0b0000 0001　負 0b0010 0000の正 三宝持つとこ開いとく為
-			//solenoidNode->Control()
-
-			while (1) {
-				//solenoidNode->Control();
-				solenoid_drive(now_pattern);
-				//carrierNode->SetCommand(CarrierCommands::deliver_l_cmd);
-				//carrierNode->Control();
-//		if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) != GPIO_PIN_RESET) {
-//			HAL_Delay(5);
-//			if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) != GPIO_PIN_RESET) {
-//				Counter_1++;
-//				switch (Counter_1) {
-//				case 1:
-//					solenoidNode->SetCommand(SolenoidCommands::picked_s_cmd); //0b0000 0001 正　0b0010 0000 負　の組み合わせをしたい
-//					solenoidNode->Control();
-//					break;
-//				case 2:
-//					solenoidNode->SetCommand(SolenoidCommands::up_s_cmd); //0b0000 0010の負論理を取る　三宝を保持してアームを曲げるところまでがここまで
-//					solenoidNode->Control();
-//					Counter_1 = 0;
-//					break;
-//				}
-//			}
-//		}
-//
-//		if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9) != GPIO_PIN_RESET) {
-//			HAL_Delay(5);
-//			if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9) != GPIO_PIN_RESET) {
-//				Counter_2++;
-//				switch (Counter_2) {
-//				case 1:
-//					solenoidNode->SetCommand(
-//							SolenoidCommands::apploaching_f_cmd); //0b0000 0100の負論理
-//					solenoidNode->Control();
-//					break;
-//				case 2:
-//					solenoidNode->SetCommand(SolenoidCommands::picked_f_1_cmd); //0b0000 1000 果物回収用のアームを下ろす
-//					solenoidNode->Control();
-//					break;
-//				case 3:
-//					solenoidNode->SetCommand(SolenoidCommands::picked_f_2_cmd); //0b0000 0100 果物回収のアームを閉じてボールつかむ
-//					solenoidNode->Control();
-//					break;
-//				case 4:
-//					solenoidNode->SetCommand(SolenoidCommands::up_f_1_cmd); //0b0000 1000の負論理　アームを上げる
-//					solenoidNode->Control();
-//					break;
-//				case 5:
-//					solenoidNode->SetCommand(SolenoidCommands::up_f_2_cmd); //0b0000 0100の負論理　ボールを離してレールへ
-//					solenoidNode->Control();
-//					Counter_2 = 0;
-//					break;
-//				}
-//			}
-//		}
-
-//			if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) != 0)			//�O��c��
-//					{
-//				Counter_1++;
-//				switch (Counter_1) {
-//				case 1:
-//					solenoidNode->SetCommand(SolenoidCommands::picked_s_cmd);//�����ɑ��肽���M�����L�q�H
-//					solenoidNode->Control();
-//					break;
-//				case 2:
-//					solenoidNode->SetCommand(SolenoidCommands::up_s_cmd);
-//					solenoidNode->Control();
-//					Counter_1 = 0;
-//					break;
-//				}
-//			}
-//
-//			if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9) != 0)			//�؂̎��c��
-//					{
-//				Counter_2++;
-//				switch (Counter_2) {
-//				case 1:
-//					solenoidNode->SetCommand(
-//							SolenoidCommands::apploaching_f_cmd);
-//					solenoidNode->Control();
-//					break;
-//				case 2:
-//					solenoidNode->SetCommand(SolenoidCommands::picked_f_1_cmd);
-//					solenoidNode->Control();
-//					break;
-//				case 3:
-//					solenoidNode->SetCommand(SolenoidCommands::picked_f_2_cmd);
-//					solenoidNode->Control();
-//					break;
-//				case 4:
-//					solenoidNode->SetCommand(SolenoidCommands::up_f_1_cmd);
-//					solenoidNode->Control();
-//					break;
-//				case 5:
-//					solenoidNode->SetCommand(SolenoidCommands::up_f_2_cmd);
-//					solenoidNode->Control();
-//					Counter_2 = 0;
-//					break;
-//				}
-//			}
-//			last_ctrl_time = HAL_GetTick();
-//		}
-
-				status = can_rx(&rx_msg, cmd);
-				if (status == HAL_OK) {
-					// received can frame
-					if (rx_msg.StdId == id_handCmd) {
-						can_unpack(cmd, order);
-						switch (order) {
-						case 0x00: //disable
-							//solenoid_disable();
-							endis = 0;
-							once = 0;
-							break;
-						case 0x01: //enable
-							//solenoid_enable();
-							endis = 1;
-							break;
-						case 0x02: //果物回収
-							//					solenoidNode->SetCommand(SolenoidCommands::apploaching_f_cmd); //0b0000 0100の正論理
-							//					solenoidNode->Control();
-							now_pattern |= 0b00000100;
-							solenoid_drive(now_pattern);
-							HAL_Delay(1000);
-							//					solenoidNode->SetCommand(SolenoidCommands::picked_f_1_cmd); //0b0000 1000 果物回収用のアームを下ろす
-							//					solenoidNode->Control();
-							now_pattern |= 0b00001000;
-							solenoid_drive(now_pattern);
-							HAL_Delay(1000);
-							//					solenoidNode->SetCommand(SolenoidCommands::picked_f_2_cmd); //0b0000 0100 負 果物回収のアームを閉じてボールつかむ
-							//					solenoidNode->Control();
-							now_pattern &= ~0b00000100;
-							solenoid_drive(now_pattern);
-							HAL_Delay(1000);
-							//					solenoidNode->SetCommand(SolenoidCommands::up_f_1_cmd); //0b0000 1000の負論理　アームを上げる
-							//					solenoidNode->Control();
-							now_pattern &= ~0b00001000;
-							solenoid_drive(now_pattern);
-							HAL_Delay(2000);
-							//					solenoidNode->SetCommand(SolenoidCommands::up_f_2_cmd); //0b0000 0100の正論理　ボールを離してレールへ
-							//					solenoidNode->Control();
-							now_pattern |= 0b00000100;
-							solenoid_drive(now_pattern);
-							break;
-						case 0x03: //親善奉納
-//					solenoidNode->SetCommand(SolenoidCommands::apploaching_a_cmd); //0b0000 0001　負 0b0010 0000の正
-//					solenoidNode->Control();
-							now_pattern &= ~0b00000001;
-							now_pattern |= 0b00100000;
-							solenoid_drive(now_pattern);
-							HAL_Delay(700); //もうちょい短くて良くない？
-
-//					solenoidNode->SetCommand(SolenoidCommands::setting_s_cmd); //0b0001 0000 最後の押し出し　いえい
-//					solenoidNode->Control();
-							now_pattern |= 0b00010000;
-							solenoid_drive(now_pattern);
-							break;
-						}
-					}
-				}
-
-				if (endis != 1) {
-					break;
-				}
-
-				if (timetosend == 1) {
-					can_tx(&tx_msg8, tx_payload8);
-//					HAL_Delay(1);
-//					can_tx(&tx_msg9, tx_payload9);
-					timetosend = 0;
- 			}
-
-				if ((HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) != GPIO_PIN_RESET)
-						&& (once != 1)) {
-					HAL_Delay(5);
-					//if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) != GPIO_PIN_RESET) {
-
-//				solenoidNode->SetCommand(SolenoidCommands::picked_s_cmd); //0b0000 0001 正　0b0010 0000 負　の組み合わせをしたい
-//				solenoidNode->Control();
-					now_pattern &= ~0b00100000;
-					now_pattern |= 0b00000001;
-					solenoid_drive(now_pattern);
-					HAL_Delay(700);
-//				solenoidNode->SetCommand(SolenoidCommands::up_s_cmd); //0b0000 0010の負論理を取る　三宝を保持してアームを曲げるところまでがここまで
-//				solenoidNode->Control();
-					now_pattern &= ~0b00000010;
-					solenoid_drive(now_pattern);
-
-					once = 1;
-					//}
-				}
-
-			}
-			if (endis != 1) {
-				break;
-			}
-		}
 	}
 }
 extern "C" void TIM2_IRQHandler(void) //送信レート100
 		{
 	if (TIM2->SR & TIM_SR_UIF) {
-		uint8_t sensor8_status;
-//		uint8_t sensor9_status;
-//		if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9) != GPIO_PIN_RESET) {
-//			sensor9_status = 1;
-//		} else {
-//			sensor9_status = 0;
-//		}
-		if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) != GPIO_PIN_RESET) {
-			sensor8_status = 1;
-		} else {
-			sensor8_status = 0;
-		}
-
-		can_pack(tx_payload8,sensor8_status);
-		timetosend =1;
-//　can packの部分がどっか行っちゃった　使わないそうなのでコメントアウト
 
 		TIM2->SR &= ~TIM_SR_UIF;
 	}
 }
-/*extern "C" void EXTI9_5_IRQHandler(void) { //おそらくcounter君をつかった処理は上手く行かないんじゃないかと　三宝回収はセンサのみ
- //	if (__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_8) != RESET) {
- //		__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_8);
- if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) != GPIO_PIN_RESET) {
- HAL_Delay(5);
- if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) != GPIO_PIN_RESET) {
 
- solenoidNode->SetCommand(SolenoidCommands::picked_s_cmd); //0b0000 0001 正　0b0010 0000 負　の組み合わせをしたい
- solenoidNode->Control();
-
- solenoidNode->SetCommand(SolenoidCommands::up_s_cmd); //0b0000 0010の負論理を取る　三宝を保持してアームを曲げるところまでがここまで
- solenoidNode->Control();
-
- }
- }
- //	if (__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_9) != RESET) {
- //		__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_9);
- //
- //			solenoidNode->SetCommand(SolenoidCommands::apploaching_f_cmd); //0b0000 0100の負論理
- //			solenoidNode->Control();
- //
- //			solenoidNode->SetCommand(SolenoidCommands::picked_f_1_cmd); //0b0000 1000 果物回収用のアームを下ろす
- //			solenoidNode->Control();
- //
- //			solenoidNode->SetCommand(SolenoidCommands::picked_f_2_cmd); //0b0000 0100 果物回収のアームを閉じてボールつかむ
- //			solenoidNode->Control();
- //
- //			solenoidNode->SetCommand(SolenoidCommands::up_f_1_cmd); //0b0000 1000の負論理　アームを上げる
- //			solenoidNode->Control();
- //
- //			solenoidNode->SetCommand(SolenoidCommands::up_f_2_cmd); //0b0000 0100の負論理　ボールを離してレールへ
- //			solenoidNode->Control();
- //
- //		}
- //	__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_9);
- EXTI->PR |= EXTI_PR_PR9;
- EXTI->PR |= EXTI_PR_PR8;
- HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_9);
- HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_8);
- }*/
 /* CAN init function */
 
 void SystemClock_Config(void) {
